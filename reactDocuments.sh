e1ca4684696314e9df7cc588d4898c7e8133fc76
Ezequiiel98 <42507863+Ezequiiel98@users.noreply.github.com>
2020-03-10 01:46:04 -0300

    update reactDocuments.sh

--- reactDocuments.sh
+++ reactDocuments.sh
@@ -1,57 +1,75 @@
 #!/bin/bash
 
-# params: $1 ruta del proyecto *obligatiorio
-          # $2 dependencias  OPCIONALEs           
-          # $3 dependencias de desarrollo OPCIONALEs
-          # $4 componentes si es que necesitamos  OPCIONALEs
-# ejemplo de uso:  ./reactDocuments.sh my-proyect 'node-sass react-router react-router-dom' 'eslint prettier eslint-plugin-react' 'componenteUno componenteDos'
+echo  "";
 
-if [ "$1" ];
-  then 
-    ruta=$1;
-    create-react-app $ruta;
-    cd $ruta/src;
+read -p 'Nombre/Ruta del proyecto a crear: ' route;
+echo  "";
+
+if [ "$route" ];
+  then  
+    read -p 'Dependencias: ' dependencies;
+    echo  "";
+    read -p 'Dependencias de desarrollo: ' devDependencies;
+    echo  "";
+    read -p 'Componentes: ' components;
+    echo "";
+    read -p "Abrir vscode y correr el proyecto react (Si/No)? " choice;
+
+    create-react-app $route;
+
+    cd $route/src;
     mkdir -p components/App assets constants;
-    mv App.js componets/App/index.js;
-    mv App.css componets/App/index.css;
+
+    mv App.css components/App/index.css;
+    #remplazo la ruta de App.css por el nuevo nombre
+    sed -i 's/\/App.css/\/index.css/g' App.js
+    cat App.js  | grep -v "logo" > components/App/index.js;
+    
     sed -i 's/\/App/\/components\/App/g' index.js;
-    head -n 8 index.js | grep -v serviceWorker > index.js;
+    #lo guardo en un archivo temporal porque si no no se puede hacer
+    head -n 8 index.js | grep -v "serviceWorker" > indexTmp;
+    mv indexTmp index.js;
     rm logo* App* se*.js;
+    
   else 
-    echo "Debe ingresar el nombre del proyecto del proyecto"
+    echo "Debe ingresar el nombre del proyecto del proyecto";
 fi
 
 #depencencies
-if [ "$2" ]; 
-  dependencies=$2;
-  then 
+if [[ ${dependencies} != '' ]]; 
+  then  
     for i in ${dependencies[@]}
       do
-       npm install $i
+      npm install $i  
     done
 fi
 
 #devDepencencies
-if [ "$3" ]; 
-  devDependencies=$3
-  then 
+if [[ ${devDependencies} != '' ]]; 
+  then  
     for i in ${devDependencies[@]}
       do
-       npm install --save-dev $i
+       npm install --save-dev $i;
     done
 fi
 
 #COMPONENTS
-if [ "$4" ]; 
-  then 
-    componets=$4
-    for i in ${componets[@]}
+if [[ ${components} != '' ]]; 
+  then  
+    for i in ${components[@]}
       do
-       mkdir components/$i;
+       mkdir -p components/$i;
        touch components/$i/index.{js,module.scss};
+       echo "import React from 'react'; " > components/$i/index.js;
     done
 fi
 
-#cd ../;
-#npm start;
-#code . 
+
+
+case "$choice" in
+  Si|si|sI|SI ) 
+        cd ../; 
+        code .; 
+        npm start ;;
+  * ) echo "" ;;
+esac
